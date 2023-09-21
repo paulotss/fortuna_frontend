e00f81df5211a5135cda98f3aa44a0d607c760cb
Paulo de Tarso <paulo.oinab@gmail.com>
2023-09-21 12:15:07 -0300

    consume endpoint login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,25 +1,77 @@
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import axios from '../http';
+
+interface Values {
+  code: string;
+  password: string;
+}
+
+const LoginSchema = Yup.object({
+  code: Yup.string().required("Obrigatório"),
+  password: Yup.string().required("Obrigatório")
+})
+
 function LoginPage () {
+  async function handleSubmit (values: Values) {
+    try {
+      const result = await axios.post('/seller/login', values);
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="code">Inscrição</label>
-          <input
-            type="text"
-            id="code"
-            name="code"
-          />
-        </div>
-        <div>
-          <label>Senha</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-          />
-        </div>
-      </form>
-    </div>
+    <main className="flex items-center flex-col mt-5">
+      <h1 className="text-xl font-bold text-yellow-600">Login</h1>
+      <div className="p-5 w-96 mt-2 rounded bg-slate-100">
+        <Formik
+          initialValues={{
+            code: "",
+            password: ""
+          }}
+          validationSchema={LoginSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className="mb-3">
+                <label htmlFor="code">Inscrição</label>
+                <br />
+                <Field
+                  id="code"
+                  name="code"
+                  className="border w-full p-1"
+                />
+                {errors.code && touched.code ? (
+                  <div className="text-xs">{errors.code}</div>
+                ) : null}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password">Senha</label>
+                <br />
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="border w-full p-1"
+                />
+                {errors.password && touched.password ? (
+                  <div className="text-xs">{errors.password}</div>
+                ) : null}
+              </div>
+              <button
+                type="submit"
+                className="p-2 bg-yellow-600 rounded w-24"
+              >
+                Entrar
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </main>
   )
 }
 
