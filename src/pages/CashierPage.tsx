1790fe4382ccc338f95d510f86ee91f5bdad2c4b
Paulo de Tarso <paulo.oinab@gmail.com>
2023-09-29 12:01:39 -0300

    add amount checkout

--- src/pages/CashierPage.tsx
+++ src/pages/CashierPage.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Dialog, DialogContent } from "@mui/material";
 import Header from "../components/Header";
+import { IconButton } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 interface IProduct {
   id: number;
   title: string;
   price: number;
   amount: number;
-  checkout?: boolean;
+  amountCheckout?: number;
+  isCheckout?: boolean;
 }
 
 const initialProducts: IProduct[] = [
@@ -19,13 +22,13 @@ const initialProducts: IProduct[] = [
   },
   {
     id: 2,
-    title: 'Produto 1',
+    title: 'Produto 2',
     price: 22,
     amount: 10
   },
   {
     id: 3,
-    title: 'Produto 1',
+    title: 'Produto 3',
     price: 23,
     amount: 10
   },
@@ -38,12 +41,37 @@ function CashierPage() {
   function handleClickProduct(id: number) {
     setProducts(products.map(product => {
       if (product.id === id) {
-        return { ...product, checkout: true }
+        return {
+          ...product,
+          amountCheckout: product.amountCheckout || 1,
+          isCheckout: true,
+        }
       } else {
         return product;
       }
     }));
     setIsDialogOpen(false);
+  }
+
+  function handleChangeAmountCheckout({ target }: ChangeEvent<HTMLInputElement>) {
+    const id = Number(target.id);
+    setProducts(products.map(product => {
+      if (product.id === id) {
+        return { ...product, amountCheckout: Number(target.value) }
+      } else {
+        return product;
+      }
+    }));
+  }
+
+  function getTotalPrice() {
+    const total = products.reduce((acc, product) => {
+      if (product.amountCheckout) {
+        return acc + (product.amountCheckout * product.price)
+      }
+      return acc;
+    }, 0);
+    return total;
   }
 
   return (
@@ -63,7 +91,7 @@ function CashierPage() {
         <div className="p-5 mb-5 border-b">
           {
             products.map(product => {
-              if (product.checkout) {
+              if (product.isCheckout) {
                 return (
                   <div
                   key={product.id}
@@ -72,13 +100,15 @@ function CashierPage() {
                     <div>{product.title}</div>
                     <div>{product.amount}</div>
                     <div>{product.price}</div>
+                    <div>{product.amountCheckout}</div>
                   </div>
                 )
               }
             })
           }
         </div>
-        <div>
+        <div className="flex justify-between">
+          <div className="text-xl font-bold">Total: { getTotalPrice() }</div>
           <button className='p-2 bg-green-600 rounded'>Finalizar</button>
         </div>
 
@@ -97,12 +127,27 @@ function CashierPage() {
               products.map((product) => (
                 <div
                   key={product.id}
-                  className="flex justify-between mt-1 p-2 bg-yellow-200 rounded cursor-pointer"
-                  onClick={() => {handleClickProduct(product.id)}}
+                  className="flex justify-between mt-1 p-2 bg-yellow-200 rounded"
                 >
                   <div>{product.title}</div>
                   <div>{product.amount}</div>
                   <div>{product.price}</div>
+                  <div>
+                    <input
+                      id={product.id.toString()}
+                      name={`checkout-${product.id}`}
+                      type="number"
+                      className="w-24"
+                      min="1"
+                      value={product.amountCheckout || 1}
+                      onChange={handleChangeAmountCheckout}
+                    />
+                  </div>
+                  <IconButton
+                    onClick={() => {handleClickProduct(product.id)}}
+                  >
+                    <AddCircleIcon />
+                  </IconButton>
                 </div>
               ))
             }
