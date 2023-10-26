efdb08c5abfb7c3f923d73762b8a70e6c2ba9c4f
Paulo de Tarso <paulo.oinab@gmail.com>
2023-10-26 16:49:22 -0300

    change menu layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate } from 'react-router-dom';
-import { IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Header() {
@@ -12,19 +11,48 @@ function Header() {
   }
 
   return (
-    <header className='w-full p-2 bg-yellow-700 flex justify-between'>
-      <div className='font-bold w-fit p-2'>
-        <Link to='/'>FORTUNA</Link>
+    <header className='w-full bg-amber-400 flex justify-start'>
+      <div className='font-bold w-20 ml-4 self-center'>
+        <Link to='/'>
+          <div className='w-8 h-8 bg-neutral-900'></div>
+        </Link>
       </div>
-      <nav className='p-2'>
-        <Link to='/cashier' className='pb-1 mr-2 hover:border-b-2 border-black'>Caixa</Link>
-        <Link to='/clients' className='pb-1 mr-2 hover:border-b-2 border-black'>Clientes</Link>
-        <Link to='/products' className='pb-1 mr-2 hover:border-b-2 border-black'>Produtos</Link>
-        <Link to='/reports' className='pb-1 mr-2 hover:border-b-2 border-black'>Relatórios</Link>
+      <nav className='self-end w-full mt-5 flex'>
+        <Link to='/cashier'>
+          <div
+            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
+          >
+            Caixa
+          </div>
+        </Link>
+        <Link to='/clients'>
+          <div
+            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
+          >
+            Clientes
+          </div>
+        </Link>
+        <Link to='/products'>
+          <div
+            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
+          >
+            Produtos
+          </div>
+        </Link>
+        <Link to='/reports'>
+          <div
+            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
+          >
+            Relatórios
+          </div>
+        </Link>
       </nav>
-      <IconButton aria-label='Logout' onClick={handleClickLogout}>
-        <LogoutIcon />
-      </IconButton>
+        <button
+          onClick={handleClickLogout}
+          className='p-2 self-center'
+        >
+          <LogoutIcon />
+        </button>
     </header>
   )
 }
--- src/components/SectionLink.tsx
+++ src/components/SectionLink.tsx
@@ -11,7 +11,7 @@ function SectionLink(props: SectionLinkProps) {
   const { icon, link, title } = props;
   return (
     <Link to={link} className=''>
-      <div className='w-32 bg-yellow-600 m-2 rounded p-5'>
+      <div className='w-32 bg-amber-400 mr-2 p-5'>
         <div className='text-2xl text-center'>{ icon }</div>
         <p className='text-xs text-center'>{ title }</p>
       </div>
