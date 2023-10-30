287f22cff2df6f6e8d95ffbc953934f629cfd028
Paulo de Tarso <paulo.oinab@gmail.com>
2023-10-30 18:37:33 -0300

    add dropdown menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,67 @@
+import React, { useState } from 'react';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { MenuItem, Menu } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          borderRadius: '0px',
+          backgroundColor: '#A3A3A3',
+          color: '#171717',
+        }
+      }
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          ":hover": {
+            backgroundColor: '#737373'
+          }
+        }
+      }
+    }
+  }
+});
+
+interface IOpenMenu {
+  cashier: boolean;
+  clients: boolean;
+  products: boolean;
+  reports: boolean;
+}
 
 function Header() {
-  const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState<IOpenMenu>({
+    cashier: false,
+    clients: false,
+    products: false,
+    reports: false,
+  })
+  const navigate = useNavigate();
+
+  function handleMenuClickItem(event: React.MouseEvent<HTMLButtonElement>) {
+    setOpen({
+      ...open,
+      [event.currentTarget.name]: true
+    })
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleMenuClose() {
+    setOpen({
+      cashier: false,
+      clients: false,
+      products: false,
+      reports: false,
+    });
+    setAnchorEl(null);
+  }
 
   function handleClickLogout() {
     sessionStorage.removeItem('auth')
@@ -18,35 +76,64 @@ function Header() {
         </Link>
       </div>
       <nav className='self-end w-full mt-5 flex'>
-        <Link to='/cashier'>
-          <div
-            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
-          >
-            Caixa
-          </div>
-        </Link>
-        <Link to='/clients'>
-          <div
-            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
-          >
-            Clientes
-          </div>
-        </Link>
-        <Link to='/products'>
-          <div
-            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
-          >
-            Produtos
-          </div>
-        </Link>
+        <button
+          type='button'
+          name='cashier'
+          onClick={handleMenuClickItem}
+          className={open.cashier ? 'menuButtonActive' : 'menuButtonInactive hover:menuButtonActive'}
+        >
+          Caixa
+        </button>
+        <button
+          type='button'
+          name='clients'
+          onClick={handleMenuClickItem}
+          className={open.clients ? 'menuButtonActive' : 'menuButtonInactive hover:menuButtonActive'}
+        >
+          Clientes
+        </button>
+        <button
+          type='button'
+          name='products'
+          onClick={handleMenuClickItem}
+          className={open.products ? 'menuButtonActive' : 'menuButtonInactive hover:menuButtonActive'}
+        >
+          Produtos
+        </button>
         <Link to='/reports'>
           <div
-            className='p-2 mr-1 w-32 bg-neutral-400 text-neutral-900 rounded-tr-lg hover:bg-neutral-900 hover:text-amber-400'
+            className='menuButtonInactive hover:menuButtonActive'
           >
             Relatórios
           </div>
         </Link>
       </nav>
+      <ThemeProvider theme={theme}>
+        <Menu
+          open={open.cashier}
+          anchorEl={anchorEl}
+          onClose={handleMenuClose}
+        >
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/cashier'}>Novo</MenuItem>
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/cashier'}>Abrir</MenuItem>
+        </Menu>
+        <Menu
+          open={open.clients}
+          anchorEl={anchorEl}
+          onClose={handleMenuClose}
+        >
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/client/new'}>Novo</MenuItem>
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/clients'}>Abrir</MenuItem>
+        </Menu>
+        <Menu
+          open={open.products}
+          anchorEl={anchorEl}
+          onClose={handleMenuClose}
+        >
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/products'}>Novo</MenuItem>
+          <MenuItem className='w-32' onClick={handleMenuClose} component={Link} to={'/products'}>Abrir</MenuItem>
+        </Menu>
+      </ThemeProvider>
         <button
           onClick={handleClickLogout}
           className='p-2 self-center'
